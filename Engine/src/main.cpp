7a86f5d4669f2f1a1d332a51b838ef1963bd25c8
Bo Thompson <Bo Thompson>
2017-01-25 20:44:17 -0500

    Initial Commit. Set up for future SFML development.

---
+++ Engine/src/main.cpp
@@ -0,0 +1,46 @@
+/**
+ * @file src\main.cpp
+ *
+ * Implements the main class.
+ */
+#ifndef _DEBUG
+#include <windows.h>
+#endif
+
+#ifdef _DEBUG
+/**
+ * Main entry-point for this application.
+ *
+ * @author Ulysee "Bo" Thompson
+ * @date 1/25/2017
+ *
+ * @param argc The number of command-line arguments provided.
+ * @param argv An array of command-line argument strings.
+ *
+ * @return Exit-code for the process - 0 for success, else an error code.
+ */
+int main(int argc, char *argv[])
+#else
+/**
+ * Window main.
+ *
+ * @author Ulysee "Bo" Thompson
+ * @date 1/25/2017
+ *
+ * @param hInstance	    The instance.
+ * @param hPrevInstance The previous instance.
+ * @param lpCmdLine	    The command line.
+ * @param nCmdShow	    The command show.
+ *
+ * @return An APIENTRY.
+ */
+int APIENTRY WinMain(
+	HINSTANCE   hInstance,
+	HINSTANCE   hPrevInstance,
+	LPSTR       lpCmdLine,
+	int         nCmdShow
+)
+#endif
+{
+	return 0;
+}
