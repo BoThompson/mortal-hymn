c97e77fc67d08d2f80f9874d8dad6ef76752bea9
Ulysee "Bo" Thompson <AimlessZealot@gmail.com>
2017-01-26 10:50:05 -0500

    Added basic support for OpenGL into program.

--- Engine/src/main.cpp
+++ Engine/src/main.cpp
@@ -6,7 +6,25 @@
 #ifndef _DEBUG
 #include <windows.h>
 #endif
+#include <GL/glew.h>
 #include <SFML/Graphics.hpp>
+#include <SFML/OpenGL.hpp>
+
+static const GLfloat points[] = {-1, 0, 0,
+								 1, 0, 0,
+								 0, 1, 0};
+GLuint vbo;
+
+
+
+void draw()
+{
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+	glDrawArrays(GL_TRIANGLES, 0, 3);
+	glDisableVertexAttribArray(0);
+}
 
 #ifdef _DEBUG
 /**
@@ -43,7 +61,24 @@ int APIENTRY WinMain(
 )
 #endif
 {
-	sf::Window window(sf::VideoMode(800, 600), "Mortal Hymn");
+	sf::ContextSettings settings;
+	int err;
+	settings.depthBits = 24;
+	settings.stencilBits = 8;
+	settings.antialiasingLevel = 4;
+	settings.majorVersion = 3;
+	settings.minorVersion = 0;
+	sf::Window window(sf::VideoMode(800, 600), "Mortal Hymn", sf::Style::Default, settings);
+	if ((err = glewInit()) != GLEW_OK)
+	{
+		printf("Error: %s\n", glewGetErrorString(err));
+		return -1;
+	}
+	glEnable(GL_DEPTH_TEST); // enable depth-testing
+	glDepthFunc(GL_LESS); // depth-testing interprets a smaller value as "closer"
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(points), points, GL_STATIC_DRAW);
 	while (window.isOpen())
 	{
 		sf::Event event;
@@ -52,6 +87,10 @@ int APIENTRY WinMain(
 			if (event.type == sf::Event::Closed)
 				window.close();
 		}
+		// clear the buffers
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		draw();
+		window.display();
 	}
 	return 0;
 }
